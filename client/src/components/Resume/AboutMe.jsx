390968b8adfedad02f94bac9e4262dab29014681
Aditya Kumar <adityaakumar.in@gmail.com>
2024-09-04 18:25:08 +0530

    updated some links

--- client/src/components/Resume/AboutMe.jsx
+++ client/src/components/Resume/AboutMe.jsx
@@ -10,19 +10,19 @@ const AboutMe = () => {
       <div className='about-me-container'>
         <div className='about-me-card'>
           <div className='about-me-info'><div className='about-me-name'>Name</div><span className='about-me-info-link'>Aditya Kumar</span></div>
-          <div className='about-me-info'><div className='about-me-name'>Email</div><span className='about-me-info-link'>adityaakumar.in@gmail.com</span></div>
+          <div className='about-me-info'><div className='about-me-name'>Email</div><span className='about-me-info-link link-hover' onClick={()=>{window.open('https://mail.google.com/mail/?view=cm&fs=1&to=adityaakumar.in@gmail.com', '_blank')}}>adityaakumar.in@gmail.com</span></div>
         </div>
         <div className='about-me-card'>
           <div className='about-me-info'><div className='about-me-name'>Experience</div><span className='about-me-info-link'>3+ Years</span></div>
-          <div className='about-me-info'><div className='about-me-name'>Instagram</div><span className='about-me-info-link'>@coding.tutor</span></div>
+          <div className='about-me-info'><div className='about-me-name'>Instagram</div><span className='about-me-info-link link-hover' onClick={()=>{window.open('https://www.instagram.com/coding.tutor/', '_blank')}}>@coding.tutor</span></div>
         </div>
         <div className='about-me-card'>
-          <div className='about-me-info'><div className='about-me-name'>Freelance</div><span className='about-me-info-link flicker'>Available</span></div>
+          <div className='about-me-info'><div className='about-me-name'>Freelance</div><span className='about-me-info-link flicker'  onClick={()=>{window.open('https://www.fiverr.com/adityadevelops/', '_blank')}}>Available</span></div>
           <div className='about-me-info'><div className='about-me-name'>Languages</div><span className='about-me-info-link'>English, Hindi</span></div>
         </div>
         <div className='about-me-card'>
           <div className='about-me-info'><div className='about-me-name'>Nationality</div><span className='about-me-info-link'>Indian</span></div>
-          <div className='about-me-info'><div className='about-me-name'>Location</div><span className='about-me-info-link'>Ghaziabad, India</span></div>
+          <div className='about-me-info'><div className='about-me-name'>Location</div><span className='about-me-info-link link-hover' onClick={()=>{window.open('https://maps.app.goo.gl/qnnc9oHsUv2D5kU18', '_blank')}}>Ghaziabad, India</span></div>
         </div>
       </div>
     </div>
